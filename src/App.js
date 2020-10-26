4f77a90492fc52a00654b9b56530dff5513bbe95
Sai-Yan-Naing <58454014+Sai-Yan-Naing@users.noreply.github.com>
2020-10-27 00:27:23 +0630

    add bootstrap and fetch api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { Container, Table, Row, Col } from 'react-bootstrap';
+
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+        items: [],
+        isLoaded: false,
+      }
+    }
+
+
+  componentDidMount()
+  {
+    fetch('https://rails-backend-api-test.herokuapp.com/api/v1/articles')
+    .then(res => res.json())
+    .then(json => {
+        this.setState({
+            isLoaded: true,
+            items: json,
+            });
+        });
+  }
+
+     render() {
+
+        var { isLoaded, items } = this.state;
+
+        if(!isLoaded){
+            return <div>Loading .......</div>;
+        }else{
+            return (
+                <div className="App">
+                    <Container className="mt-5">
+                    <div className="text-center display-4"><b>React JS Fetch Data from Rail API</b></div>
+                        <ul>
+                            {items.data.map( item => (
+                                <li key={item.id}>{item.title}</li>
+                                ))}
+                        </ul>
+                        <Row>
+                            <Col sm={6}>Status => {items.status}</Col>
+                            <Col sm={6}>Message =>  {items.message}</Col>
+                        </Row>
+                        <Table  striped bordered hover className="mt-2" size="sm">
+                              <thead>
+                                <tr>
+                                  <th>No</th>
+                                  <th>Title</th>
+                                  <th>Body</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                              {items.data.map( item => (
+
+                                <tr key={item.id} >
+                                  <td>{item.id}</td>
+                                  <td>{item.title}</td>
+                                  <td>{item.body}</td>
+                                </tr>
+                                ))}
+
+                              </tbody>
+                            </Table>
+                    </Container>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
